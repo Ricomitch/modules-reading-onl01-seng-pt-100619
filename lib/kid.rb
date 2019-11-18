2f43bddc9394344b2b865d8baeea8a966e3e39ae
Rico Mitchell <ricomitch@icloud.com>
2019-11-18 01:42:14 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,14 +1,14 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid
-  include Dance
-  
   class Kid
   extend MetaDancing
   end
- 
-  attr_accessor :name
+  
+  class Kid
+    include Dance
+  
+ attr_accessor :name
  
   def initialize(name)
     @name = name
