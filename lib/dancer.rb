64b50369beec487d97de80f4a7d9aad3cff0b2be
Rico Mitchell <ricomitch@icloud.com>
2019-11-18 01:35:02 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,11 +1,12 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dancer
-  include Dance
+  class Dancer
+    include Dance
+  end  
   
-   class Dancer
-  extend MetaDancing
+  class Dancer
+    extend MetaDancing
   end
  
   attr_accessor :name
