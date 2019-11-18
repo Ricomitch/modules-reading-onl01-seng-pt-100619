4135bc9cc89ce48f2dc8ae4ccbf4a7c0ba453743
Rico Mitchell <ricomitch@icloud.com>
2019-11-18 01:54:30 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,12 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
   class Kid
-  extend MetaDancing
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+  end
+  
+  class Kid
+    extend MetaDancing
   end
   
   class Kid
